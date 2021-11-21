7f57333a8c96a76acb7eaff7b946fda8348e793c
Michał Dobosz <m.dobosz@ideo.pl>
2021-11-21 23:38:04 +0100

    updated comment resource to return id

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -15,6 +15,7 @@ class CommentResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'content' => $this->content,
             'user' => UserResource::make($this->user)
         ];
